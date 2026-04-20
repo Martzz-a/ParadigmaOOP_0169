001547aa85779820d14169924c1e9ae1be5883a0
Martzz-a <maritzafidelyah@gmail.com>
2026-04-20 14:55:17 +0700

    Menambahkan Program Utama

--- pratikumOOP.cpp
+++ pratikumOOP.cpp
@@ -16,3 +16,20 @@ public:
         cout << endl;
     }
 };
+
+int main(){
+    Barang elektronik;
+    Barang nonElektronik;
+
+    elektronik.nama = "Mesin Cuci";
+    elektronik.jumlah = 110;
+    elektronik.kategori = "Elektronik";
+    elektronik.tanggalProduksi = "2025-31-12";
+    elektronik.printData();
+
+    nonElektronik.nama = "Meja";
+    nonElektronik.jumlah = 324;
+    nonElektronik.kategori = "Non-Elektronik";
+    nonElektronik.tanggalProduksi = "2025-31-12";
+    nonElektronik.printData();
+}
