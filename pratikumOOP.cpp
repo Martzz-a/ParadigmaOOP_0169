dee124e2642d9e927fe9ac756bcae27f7feb5c06
Martzz-a <maritzafidelyah@gmail.com>
2026-04-20 14:54:49 +0700

    Menambahkan Class Barang

--- pratikumOOP.cpp
+++ pratikumOOP.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+class Barang{
+public:
+    string nama;
+    int jumlah;
+    string kategori;
+    string tanggalProduksi;
+
+    void printData(){
+        cout << "Nama             : " << nama << endl;
+        cout << "Jumlah           : " << jumlah << endl;
+        cout << "Kategori         : " << kategori << endl;
+        cout << "Tanggal Produksi : " << tanggalProduksi << endl;
+        cout << endl;
+    }
+};
